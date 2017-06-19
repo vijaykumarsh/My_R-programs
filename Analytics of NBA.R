dcd5f5ba642c57604e3c21411bd9ed7b9cefbd05
vijaykumar <vhadalgi@gmail.com>
2017-06-19 14:47:12 +0530

    Add files via upload

---
+++ Analytics of NBA.R
@@ -0,0 +1,28 @@
+
+myplot(Salary)
+myplot(Salary/Games)
+myplot(MinutesPlayed)
+
+#accuracy
+
+myplot(FieldGoals/FieldGoalAttempts)
+
+#intersting observation
+
+myplot(MinutesPlayed/Games)
+
+
+#Player style
+
+myplot
+
+
+f <- function(vector=1:3){
+  
+  vector * 5
+  
+}
+
+f()+f(c(1,1,1))
+
+f
